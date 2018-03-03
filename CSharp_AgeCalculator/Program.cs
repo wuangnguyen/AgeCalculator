5b37907be8b16bb2dd017ad1cd977804f0662988
Quang <quang.nnd.hcmus@gmail.com>
2018-03-03 16:12:56 +0700

    C# Age Calculator

---
+++ CSharp_AgeCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AgeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime bday = new DateTime(1991, 06, 21);
+            DateTime cday = DateTime.Today;
+            Age age = new Age(bday, cday);
+            Console.WriteLine(age.ToString());
+            Console.ReadLine();
+        }
+    }
+}
